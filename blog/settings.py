4c0ab122d98db3e8ec86bcb8337cb3f61b8f5143
Ubuntu <ubuntu@ip-172-31-83-74.ec2.internal>
2019-07-24 01:23:10 +0000

    typo in whitenoisemiddleware

--- blog/settings.py
+++ blog/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleWare',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
